6b7c1b0b780b23ee3c32b3ea3baaa1c25cbaeb42
Joshua Herman <joshsimages@gmail.com>
2020-07-17 10:18:17 -0600

    eslint
    linted all files

--- packages/landingpage/components/layout.tsx
+++ packages/landingpage/components/layout.tsx
@@ -2,13 +2,14 @@ import Header from './header'
 import Footer from './footer'
 import Head from 'next/head'
 
-function Layout (props) {
+export default function Layout (props: { children: import("react").ReactNode }) {
   return (
     <>
       <div>
         <Head>
           <link rel='icon' type='image/x-icon' href='favicon.ico' />
           <title>My New Website</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <Header />
         <main className='flex-1 w-full max-w-6xl p-1 mx-auto md:px-8 md:py-2'>
@@ -19,5 +20,3 @@ function Layout (props) {
     </>
   )
 }
-
-export default Layout
